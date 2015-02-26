6dedf4ef08da48cd2e4a7a8f6ce1c77459f2dc27
Umut Bozkurt <umut@hipo.biz>
2015-02-26 21:19:10 +0200

    inject setting to DRF settings

--- timestamp_pagination/serializers.py
+++ timestamp_pagination/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework.pagination import BasePaginationSerializer, NextPageField, PreviousPageField
 from rest_framework.templatetags.rest_framework import replace_query_param
+from rest_framework.settings import api_settings
+
 
 
 class MaxTimestampField(PreviousPageField):
@@ -9,14 +11,30 @@ class MaxTimestampField(PreviousPageField):
     def to_representation(self, value):
         if not value.has_next():
             return None
+
         page = value.next_page_number()
         request = self.context.get('request')
-        url = request and request.build_absolute_uri() or ''
+
+        use_absolute_url = getattr(api_settings, 'PAGINATION_USE_ABSOLUTE_URL', None)
+        url = use_absolute_url and request and request.build_absolute_uri() or ''
+
         return replace_query_param(url, self.page_field, page)
 
 
 class MinTimestampField(NextPageField):
     page_field = 'min_timestamp'
+
+    def to_representation(self, value):
+        if not value.has_previous():
+            return None
+
+        page = value.previous_page_number()
+        request = self.context.get('request')
+
+        use_absolute_url = getattr(api_settings, 'PAGINATION_USE_ABSOLUTE_URL', None)
+        url = use_absolute_url and request and request.build_absolute_uri() or ''
+
+        return replace_query_param(url, self.page_field, page)
 
 
 class TimestampPaginationSerializer(BasePaginationSerializer):
