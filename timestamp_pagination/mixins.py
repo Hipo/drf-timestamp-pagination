ff1073983db50a9f86e41565bfc4ee94e077f03e
Umut Bozkurt <umut@hipo.biz>
2015-02-27 13:26:24 +0200

    refactor for paginator changes

---
+++ timestamp_pagination/mixins.py
@@ -0,0 +1,24 @@
+
+
+class TimestampPaginationMixin(object):
+    timestamp_field = None
+    next_timestamp_kwarg = 'next'
+
+    def paginate_queryset(self, queryset):
+        """
+        Paginate a queryset if required, either returning a page object,
+        or `None` if pagination is not configured for this view.
+        """
+        page_size = self.get_paginate_by()
+        if not page_size:
+            return None
+
+        assert self.timestamp_field is not None, '`timestamp_field` must be specified for TimestampPaginator to paginate'
+
+        paginator = self.paginator_class(queryset, 'timestamp', page_size)
+
+        next_timestamp_kwarg = self.kwargs.get(self.next_timestamp_kwarg)
+        next_timestamp_query_param = self.request.query_params.get(self.next_timestamp_kwarg)
+        next_timestamp = next_timestamp_kwarg or next_timestamp_query_param
+
+        return paginator.page(next_timestamp)
--- timestamp_pagination/serializers.py
+++ timestamp_pagination/serializers.py
@@ -1,34 +1,16 @@
-from rest_framework.pagination import BasePaginationSerializer, NextPageField, PreviousPageField
+from rest_framework.pagination import BasePaginationSerializer, NextPageField
 from rest_framework.templatetags.rest_framework import replace_query_param
 from rest_framework.settings import api_settings
 
 
-
-class MaxTimestampField(PreviousPageField):
-    page_field = 'max_timestamp'
-    #TODO: add flag for absolute uri (inject to DRF settings)
+class NextTimestampField(NextPageField):
+    page_field = 'next'
 
     def to_representation(self, value):
         if not value.has_next():
             return None
 
-        page = value.next_page_number()
-        request = self.context.get('request')
-
-        use_absolute_url = getattr(api_settings, 'PAGINATION_USE_ABSOLUTE_URL', None)
-        url = use_absolute_url and request and request.build_absolute_uri() or ''
-
-        return replace_query_param(url, self.page_field, page)
-
-
-class MinTimestampField(NextPageField):
-    page_field = 'min_timestamp'
-
-    def to_representation(self, value):
-        if not value.has_previous():
-            return None
-
-        page = value.previous_page_number()
+        page = value.next_page_timestamp()
         request = self.context.get('request')
 
         use_absolute_url = getattr(api_settings, 'PAGINATION_USE_ABSOLUTE_URL', None)
@@ -41,5 +23,4 @@ class TimestampPaginationSerializer(BasePaginationSerializer):
     """
 
     """
-    min_timestamp = MinTimestampField(source='*')
-    max_timestamp = MaxTimestampField(source='*')
+    next = NextTimestampField(source='*')
