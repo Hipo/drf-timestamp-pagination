6812162260460c980385c3a56a5240d1896c762e
Umut Bozkurt <umut@hipo.biz>
2015-02-27 17:12:06 +0200

    serializer refactor and new version

--- drf_timestamp_pagination/serializers.py
+++ drf_timestamp_pagination/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework.pagination import BasePaginationSerializer, NextPageField
 from rest_framework.templatetags.rest_framework import replace_query_param
-from rest_framework.settings import api_settings
 
 
 class NextTimestampField(NextPageField):
@@ -12,15 +11,13 @@ class NextTimestampField(NextPageField):
 
         page = value.next_page_timestamp()
         request = self.context.get('request')
-
-        use_absolute_url = getattr(api_settings, 'PAGINATION_USE_ABSOLUTE_URL', None)
-        url = use_absolute_url and request and request.build_absolute_uri() or ''
+        url = request and request.build_absolute_uri() or ''
 
         return replace_query_param(url, self.page_field, page)
 
 
 class TimestampPaginationSerializer(BasePaginationSerializer):
     """
-
+    Pagination serializer to work with TimestampPaginator
     """
     next = NextTimestampField(source='*')
